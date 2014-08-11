de50f69a2f6a5e59c11e6c19ff291a576f43d07f
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-08-11 13:12:23 +0100

    Create localfile.js

---
+++ localfile.js
@@ -0,0 +1,9 @@
+angular.module('localfile', [])
+  .controller('localFileController', ['$scope', function($scope) {
+    $scope.files = [];
+    
+    for(var i = 0; i < localStorage.length; i++){ 
+      $scope.files.push(localStorage.key(i))
+    }
+
+  }]);
