411ba01b413fac4eafe9f1cc59e59b70172a5276
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-08-11 14:10:52 +0100

    Update localfile.js

--- localfile.js
+++ localfile.js
@@ -1,12 +1,21 @@
   
 var f = {"one":"two"};
 
+var m = {
+    "India": "2",
+    "England": "2",
+    "Brazil": "3",
+    "UK": "1",
+    "USA": "3",
+    "Syria": "2"
+};
+
 for(i = 0; i < localStorage.length; i++)
 {
   f[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
 }
   
 function fileCtrl($scope) {
- $scope.files = f;
+ $scope.files = m;
 }
 
