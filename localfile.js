79fe850895235baecfc10b6b5e53b16ce6730b22
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-08-13 10:02:48 +0100

    Update localfile.js

--- localfile.js
+++ localfile.js
@@ -1,12 +1,19 @@
- var f = {};
+ var f = [];//{};
  
 for(i = 0; i < localStorage.length; i++)
 {
-  f[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
+  //f[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
+  f.push(localStorage.key(i));
 }
  
  angular.module('app', []).controller('fileCtrl', function fileCtrl($scope) {
          $scope.files = f;
+         $scope.add = function(item) {
+          $scope.files.push(item);
+         };
+         $scope.remove = function(index) {
+          data.push($scope.files.splice(index, 1)[0]);
+         };
     }); 
 
 
