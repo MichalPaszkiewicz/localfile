17a977ae30c4532fed6fedecfb5dd4b34c385a57
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-08-21 12:49:40 +0100

    Update localfile.js

--- localfile.js
+++ localfile.js
@@ -56,7 +56,7 @@ angular.module('app', []).controller('fileCtrl', function fileCtrl($scope, fileF
     $scope.files = f;
     $scope.currentFileName = "Untitled";
     $scope.open = function(index){
-      currentFileName = $scope.files[index].name;
+      $scope.currentFileName = $scope.files[index].name;
       
       $('.file-name').attr('contenteditable','false');
       $(".modal-box").removeClass("hidden");
